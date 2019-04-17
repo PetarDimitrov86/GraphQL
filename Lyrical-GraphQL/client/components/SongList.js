0674eba730f40dc47605e5babc01ae50223701a3
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-17 11:52:16 +0300

    Use ApolloProvider as root wrapper. Add first component with gql query

---
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+  renderSongs() {
+    if (this.props.data.loading) {
+      return <span>Loading...</span>;
+    }
+
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className='collection-item'>
+          {song.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return <ul className='collection'>{this.renderSongs()}</ul>;
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
