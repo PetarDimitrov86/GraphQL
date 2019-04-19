254400fdffbece4debf6074005d5f56ec108d3e7
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-19 16:58:23 +0300

    Implement basic client structure

--- auth-graphql-starter/server/schema/types/user_type.js
+++ auth-graphql-starter/server/schema/types/user_type.js
@@ -1,11 +1,12 @@
 const grpahql = require('graphql');
 const {
-  GraphQLObjectType, GraphQLString
+  GraphQLObjectType, GraphQLString, GraphQLID
 } = grpahql;
 
 const UserType = new GraphQLObjectType({
   name: 'UserType',
   fields: {
+    id: { type: GraphQLID },
     email: { type: GraphQLString }
   }
 });
