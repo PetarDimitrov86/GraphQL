6ad18ae9d9c6139a9e7184048c56439d8ab6ef1e
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-19 19:10:26 +0300

    Add auth form and login component. Add login mutation

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -5,6 +5,7 @@ import { ApolloClient, createNetworkInterface } from 'apollo-client';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
 import App from './components/App';
+import LoginForm from './components/LoginForm';
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql', 
@@ -23,7 +24,7 @@ const Root = () => {
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path='/' component={App}>
-          hui
+          <Route path='login' component={LoginForm}/>
         </Route>
       </Router>
     </ApolloProvider>
