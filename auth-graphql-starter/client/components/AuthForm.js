37ae0b2dd9cb4d9e0e2159fa2458d776586e691f
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-22 13:26:51 +0300

    Add simple error message display and validation

--- auth-graphql-starter/client/components/AuthForm.js
+++ auth-graphql-starter/client/components/AuthForm.js
@@ -11,6 +11,7 @@ class AuthForm extends Component {
     event.preventDefault();
 
     this.props.onSubmit(this.state);
+    this.setState({ password: '' });
   }
 
   render() {
@@ -31,6 +32,11 @@ class AuthForm extends Component {
               value={this.state.password}
               onChange={e => this.setState({ password: e.target.value })}
             />
+          </div>
+          <div className='errors'>
+            {this.props.errors.map(error => (
+              <div key={error}>{error}</div>
+            ))}
           </div>
           <button className='btn'>Submit</button>
         </form>
